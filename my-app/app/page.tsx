611e1673e3e19a521eb9b39bbcdf2f0ce080fd0e
Álvaro Careli <122116260+alvarosamp@users.noreply.github.com>
2026-03-24 11:04:17 -0300

    up

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -6,11 +6,11 @@ import Photo from "@/components/Photo";
 const Home = () => {
   return ( <section className = 'h-full'>
     <div className = 'container mx-auto'>
-      <div className = 'flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24'>
+      <div className = 'flex flex-col xl:flex-row items-center justify-between xl:pt-2 xl:pb-12'>
         {/* text */}
-        <div className = 'text-center xl:text-left order-2 xl:order-none'>
+        <div className = 'text-center xl:text-left order-2 xl:order-none mt-[-32px] xl:mt-[-48px]'>
           <span className= 'text-xl'>AI Engineer</span>
-          <h1 className = 'h1 mb-6'>
+          <h1 className = 'h1 mb-4'>
             Hello I'm <br /><span className='text-accent'> Álvaro Sampaio</span>
           </h1>
           <p className = 'max-w-[500px] mb-9 text-white/80'>
--- my-app/components/Header.jsx
+++ my-app/components/Header.jsx
@@ -12,7 +12,7 @@ const Header = () => {
                 {/* logo */}
                 <Link href = "/">
                 <h1 className = 'text-4xl font-semibold'>
-                    Alvaro <span className = 'text-accent'>.</span>
+                    Álvaro <span className = 'text-accent'>.</span>
                 </h1>
                 </Link>
                 {/* Desktop nav */}
